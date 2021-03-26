73c415e82024106ecbe0271b581655f6fc97918d
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-03-26 18:42:26 -0400

    Sindhu - Created add-recipe-page.js file

---
+++ src/components/add-recipe-page/add-recipe-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const AddRecipePage = () => {
+    return <div>
+        <h1>Add Recipe Page</h1>
+        <Link to={`/`}>
+
+        </Link>
+    </div>
+}
+
+export default AddRecipePage
