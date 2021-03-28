0dea2479f472632256e57d15cb5878df1353e0fe
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-03-28 01:39:01 -0400

    Sindhu - including search page reducer in website-manager.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 function App() {
   return (
       <BrowserRouter>
-          <div className="container-fluid">
+          <div>
               <WebManager/>
           </div>
       </BrowserRouter>
--- src/components/website-manager.js
+++ src/components/website-manager.js
@@ -5,18 +5,25 @@ import LoginPage from "./login-page/login-page";
 import RecipePage from "./recipe-page/recipe-page";
 import DummyHomePage from "./dummy-home-page";
 import recipePageReducer from "../reducers/recipe-page-reducer"
-import {createStore} from "redux";
+import searchRecipeReducer from "../reducers/search-recipe-reducer";
+import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import CreateRecipe from "./add-recipe-page/create-recipe";
 import Profile from "./profile/profile";
+import SearchRecipe from "./search-recipe/search-recipe";
 
-const store = createStore(recipePageReducer)
+const reducer = combineReducers({
+    recipePageReducer: recipePageReducer,
+    searchRecipeReducer: searchRecipeReducer
+})
+
+const store = createStore(reducer)
 
 const WebManager = () => {
     return (
         <Provider store={store}>
             <div>
-                <Redirect from={"/"} to={"/welcome"}/>
+                {/*<Redirect from={"/"} to={"/welcome"}/>*/}
                 <Route path="/welcome">
                     <LandingPage/>
                 </Route>
@@ -34,6 +41,9 @@ const WebManager = () => {
                 </Route>
                 <Route path="/:username/profile">
                     <Profile/>
+                </Route>
+                <Route path="/search">
+                    <SearchRecipe/>
                 </Route>
             </div>
         </Provider>
