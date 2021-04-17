73541e439ae736fe5ded933f5142143acd40bf9a
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-17 19:15:46 +0530

    Sindhu - created privacy policy file

---
+++ src/components/privacy/privacy-policy.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from "@material-ui/core/Container";
+
+const PrivacyPolicy = () => {
+    return <div>
+        <Container>
+
+        </Container>
+    </div>
+}
+
+export default PrivacyPolicy
